362f4cc16fcbd2a281d0620f2b5032f0d82e778a
Anatoly Ivanov <java@inheaven.ru>
2017-11-21 17:02:51 +0700

    init css theme

--- jedani-user/src/main/java/ru/complitex/jedani/web/HomePage.java
+++ jedani-user/src/main/java/ru/complitex/jedani/web/HomePage.java
@@ -1,10 +1,19 @@
 package ru.complitex.jedani.web;
 
+import org.apache.wicket.markup.head.CssHeaderItem;
+import org.apache.wicket.markup.head.IHeaderResponse;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.request.resource.PackageResourceReference;
 
 /**
  * @author Anatoly A. Ivanov
  * 21.11.2017 15:04
  */
 public class HomePage extends WebPage{
+    @Override
+    public void renderHead(IHeaderResponse response) {
+        super.renderHead(response);
+
+        response.render(CssHeaderItem.forReference(new PackageResourceReference(HomePage.class, "/css/jedani.css")));
+    }
 }
--- jedani-user/src/main/java/ru/complitex/jedani/web/JedaniWebApplication.java
+++ jedani-user/src/main/java/ru/complitex/jedani/web/JedaniWebApplication.java
@@ -1,6 +1,13 @@
 package ru.complitex.jedani.web;
 
+import de.agilecoders.wicket.core.Bootstrap;
+import de.agilecoders.wicket.core.settings.BootstrapSettings;
+import de.agilecoders.wicket.core.settings.IBootstrapSettings;
+import de.agilecoders.wicket.core.settings.SingleThemeProvider;
+import de.agilecoders.wicket.less.BootstrapLess;
+import de.agilecoders.wicket.themes.markup.html.google.GoogleTheme;
 import org.apache.wicket.Page;
+import org.apache.wicket.cdi.CdiConfiguration;
 import org.apache.wicket.protocol.http.WebApplication;
 
 /**
@@ -10,5 +17,26 @@ import org.apache.wicket.protocol.http.WebApplication;
 public class JedaniWebApplication extends WebApplication{
     public Class<? extends Page> getHomePage() {
         return HomePage.class;
+    }
+
+    @Override
+    protected void init() {
+        new CdiConfiguration().configure(this);
+
+        configureBootstrap();
+        configureMount();
+    }
+
+    private void configureBootstrap() {
+        IBootstrapSettings settings = new BootstrapSettings();
+        Bootstrap.builder().withBootstrapSettings(settings).install(this);
+
+        settings.setThemeProvider(new SingleThemeProvider(new GoogleTheme()));
+
+        BootstrapLess.install(this);
+    }
+
+    private void configureMount() {
+
     }
 }
