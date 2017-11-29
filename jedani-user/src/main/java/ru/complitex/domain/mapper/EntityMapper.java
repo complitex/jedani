5fbeaa69e802c80636e1093da60b7fa3d8349c93
Anatoly Ivanov <java@inheaven.ru>
2017-11-29 17:57:58 +0700

    dev domain service

---
+++ jedani-user/src/main/java/ru/complitex/domain/mapper/EntityMapper.java
@@ -0,0 +1,8 @@
+package ru.complitex.domain.mapper;
+
+/**
+ * @author Anatoly A. Ivanov
+ * 29.11.2017 17:54
+ */
+public class EntityMapper {
+}
---
+++ jedani-user/src/main/java/ru/complitex/domain/service/DomainService.java
@@ -0,0 +1,9 @@
+package ru.complitex.domain.service;
+
+/**
+ * @author Anatoly A. Ivanov
+ * 29.11.2017 17:54
+ */
+public class DomainService {
+    //todo dev time-varying entity value storage service
+}
