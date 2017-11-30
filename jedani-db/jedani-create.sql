45a72f92e3d97d3fb84602397c3094ee729219d2
Anatoly Ivanov <java@inheaven.ru>
2017-11-30 15:22:52 +0700

    add common entity create sql script

--- jedani-db/jedani-create.sql
+++ jedani-db/jedani-create.sql
@@ -1,56 +1,333 @@
-create table region
-(
-  id int not null primary key,
-  name varchar(255) not null,
-  manager_id int null
-);
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+-- --------------------------------------------------------------------------------------------------------------------
+-- Common
+-- --------------------------------------------------------------------------------------------------------------------
 
-create table city
-(
-  id int not null primary key,
-  name varchar(255) not null,
-  region_id int null,
-  manager_id int null,
-  constraint city__region_fk
-  foreign key (region_id) references region (id)
-);
+-- ------------------------------
+-- User
+-- ------------------------------
+DROP TABLE IF EXISTS `user`;
 
-create index city__region_fk on city (region_id);
+CREATE TABLE  `user` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор пользователя',
+  `login` VARCHAR(64) NOT NULL COMMENT 'Имя пользователя',
+  `password` VARCHAR(64) NOT NULL COMMENT 'Пароль',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_key_login` (`login`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Пользователь';
 
-create table user
-(
-  id int not null primary key,
-  email varchar(255) not null null,
-  encrypted_password varchar(255) not null,
-  ancestry varchar(255) null,
-  j_id varchar(255) null,
-  reset_password_token varchar(255) null,
-  reset_password_send_at datetime null,
-  remember_created_at datetime null,
-  created_at datetime null,
-  updated_at datetime null,
-  mk_status int null,
-  first_name varchar(255) null,
-  second_name varchar(255) null,
-  last_name varchar(255) null,
-  phone varchar(255) null,
-  city_id int null,
-  manager_rank_id int null,
-  involved_at datetime null,
-  full_ancestry_path varchar(255) null,
-  depth_level varchar(255) null,
-  ancestry_depth int null,
-  contact_info varchar(255) null,
-  birthday date null,
-  fired_status tinyint null,
-  old_parent_id int null,
-  old_child_id text null,
-  constraint user__city_fk
-  foreign key (city_id) references city (id)
-);
+-- ------------------------------
+-- Usergroup
+-- ------------------------------
+DROP TABLE IF EXISTS `usergroup`;
 
-create index user__city_fk on user (city_id);
+CREATE TABLE  `usergroup` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор группы пользователей',
+  `login` VARCHAR(45) NOT NULL COMMENT 'Имя пользователя',
+  `group_name` VARCHAR(45) NOT NULL COMMENT 'Название группы',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_login__group_name` (`login`, `group_name`),
+  CONSTRAINT `fk_usergroup__user` FOREIGN KEY (`login`) REFERENCES `user` (`login`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Группа пользователей';
 
+-- ------------------------------
+-- Sequence
+-- ------------------------------
+
+DROP TABLE IF EXISTS `sequence`;
+CREATE TABLE `sequence`(
+  `name` VARCHAR(100) NOT NULL COMMENT 'Название таблицы сущности',
+  `value` bigint UNSIGNED NOT NULL DEFAULT '0' COMMENT 'Значение идентификатора',
+  PRIMARY KEY (`name`)
+)ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Генератор идентификаторов объектов';
+
+-- ------------------------------
+-- Locale
+-- ------------------------------
+
+DROP TABLE IF EXISTS `locale`;
+CREATE TABLE `locale` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор локали',
+  `locale` VARCHAR(2) NOT NULL COMMENT 'Код локали',
+  `system` TINYINT(1) NOT NULL default 0 COMMENT 'Является ли локаль системной',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_key_locale` (`locale`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Локаль';
+
+-- ------------------------------
+-- Entity
+-- ------------------------------
+
+DROP TABLE IF EXISTS `entity_string_value`;
+CREATE TABLE `entity_string_value` (
+  `pk_id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Суррогатный ключ',
+  `id` BIGINT(20) NOT NULL COMMENT 'Идентификатор локализации',
+  `locale_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор локали',
+  `value` VARCHAR(1000) COMMENT 'Текстовое значение',
+  PRIMARY KEY (`pk_id`),
+  UNIQUE KEY `unique_id__locale` (`id`, `locale_id`),
+  KEY `key_locale` (`locale_id`),
+  KEY `key_value` (`value`(128)),
+  CONSTRAINT `fk_string_value__locale` FOREIGN KEY (`locale_id`) REFERENCES `locale` (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Локализация';
+
+DROP TABLE IF EXISTS `entity`;
+CREATE TABLE `entity` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор сущности',
+  `entity` VARCHAR(100) NOT NULL COMMENT 'Название сущности',
+  `name_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор локализации названия сущности',
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `unique_entity` (entity),
+  KEY `key_name_id` (`name_id`),
+  CONSTRAINT `fk_entity__entity_string_value` FOREIGN KEY (name_id) REFERENCES entity_string_value (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Сущность';
+
+DROP TABLE IF EXISTS entity_value_type;
+CREATE TABLE `entity_value_type` (
+  `id` BIGINT(20) NOT NULL COMMENT 'Идентификатор типа значения атрибута',
+  `value_type` VARCHAR(100) NOT NULL COMMENT 'Тип значения атрибута',
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Тип значения атрибута';
+
+DROP TABLE IF EXISTS `entity_attribute`;
+CREATE TABLE `entity_attribute` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор типа атрибута',
+  `entity_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор сущности',
+  `name_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор локализации названия атрибута',
+  `start_date` TIMESTAMP NOT NULL default CURRENT_TIMESTAMP COMMENT 'Дата начала периода действия типа атрибута',
+  `end_date` TIMESTAMP NULL default NULL COMMENT 'Дата окончания периода действия типа атрибута',
+  `value_type_id` BIGINT(20) COMMENT  'Тип значения атрибута',
+  `reference_id` BIGINT(20) COMMENT  'Внешний ключ',
+  `system` TINYINT(1) default 0 NOT NULL COMMENT 'Является ли тип атрибута системным',
+  `required` TINYINT(1) default 0 NOT NULL COMMENT 'Является ли атрибут обязательным',
+  PRIMARY KEY (`id`),
+  KEY `key_entity_id` (`entity_id`),
+  KEY `key_name_id` (`name_id`),
+  KEY `key_value_id` (`value_type_id`),
+  CONSTRAINT `fk_attribute_type__entity` FOREIGN KEY (`entity_id`) REFERENCES `entity` (`id`),
+  CONSTRAINT `fk_entity_attribute__entity_string_value` FOREIGN KEY (`name_id`) REFERENCES entity_string_value (`id`),
+  CONSTRAINT `fk_entity_attribute__entity_value_type` FOREIGN KEY (`value_type_id`) REFERENCES entity_value_type (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Тип атрибута сущности';
+
+-- ------------------------------
+-- Permission
+-- ------------------------------
+
+DROP TABLE IF EXISTS `permission`;
+CREATE TABLE `permission` (
+  `pk_id` BIGINT(20) NOT NULL AUTO_INCREMENT  COMMENT 'Суррогатный ключ',
+  `permission_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор права доступа',
+  `table` VARCHAR(64) NOT NULL COMMENT 'Таблица',
+  `entity` VARCHAR(64) NOT NULL COMMENT 'Сущность',
+  `object_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор объекта',
+  PRIMARY KEY (`pk_id`),
+  UNIQUE KEY `key_unique` (`permission_id`, `entity`, `object_id`),
+  KEY `key_permission_id` (`permission_id`),
+  KEY `key_table` (`table`),
+  KEY `key_entity` (`entity`),
+  KEY `key_object_id` (`object_id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Права доступа';
+
+-- --------------------------------------------------------------------------------------------------------------------
+-- Address
+-- --------------------------------------------------------------------------------------------------------------------
+
+-- ------------------------------
+-- Region
+-- ------------------------------
+DROP TABLE IF EXISTS `region`;
+CREATE TABLE `region` (
+  `pk_id` BIGINT(20) NOT NULL AUTO_INCREMENT  COMMENT 'Суррогатный ключ',
+  `object_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор объекта',
+  `parent_id` BIGINT(20) COMMENT 'Идентификатор родительского объекта',
+  `parent_entity_id` BIGINT(20) COMMENT 'Идентификатор сущности родительского объекта',
+  `start_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата начала периода действия объекта',
+  `end_date` TIMESTAMP NULL DEFAULT NULL COMMENT 'Дата окончания периода действия объекта',
+  `status` INTEGER NOT NULL DEFAULT 1 COMMENT 'Статус',
+  `permission_id` BIGINT(20) NOT NULL DEFAULT 0 COMMENT 'Ключ прав доступа к объекту',
+  `external_id` VARCHAR(20) COMMENT 'Внешний идентификатор импорта записи',
+  PRIMARY KEY  (`pk_id`),
+  UNIQUE KEY `unique_object_id__start_date` (`object_id`,`start_date`),
+  UNIQUE KEY `unique_external_id` (`external_id`,`start_date`),
+  KEY `key_object_id` (`object_id`),
+  KEY `key_parent_id` (`parent_id`),
+  KEY `key_parent_entity_id` (`parent_entity_id`),
+  KEY `key_start_date` (`start_date`),
+  KEY `key_end_date` (`end_date`),
+  KEY `key_status` (`status`),
+  KEY `key_permission_id` (`permission_id`),
+  CONSTRAINT `fk_region__entity` FOREIGN KEY (`parent_entity_id`) REFERENCES `entity` (`id`),
+  CONSTRAINT `fk_region__permission` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`permission_id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Регион';
+
+DROP TABLE IF EXISTS `region_attribute`;
+CREATE TABLE `region_attribute` (
+  `pk_id` BIGINT(20) NOT NULL AUTO_INCREMENT  COMMENT 'Суррогатный ключ',
+  `attribute_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор атрибута',
+  `object_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор объекта',
+  `entity_attribute_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор типа атрибута',
+  `value_id` BIGINT(20) COMMENT 'Идентификатор значения',
+  `start_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата начала периода действия атрибута',
+  `end_date` TIMESTAMP NULL DEFAULT NULL COMMENT 'Дата окончания периода действия атрибута',
+  `status` INTEGER NOT NULL DEFAULT 1 COMMENT 'Статус',
+  PRIMARY KEY  (`pk_id`),
+  UNIQUE KEY `unique_id` (`attribute_id`,`object_id`,`entity_attribute_id`, `start_date`),
+  KEY `key_object_id` (`object_id`),
+  KEY `key_entity_attribute_id` (`entity_attribute_id`),
+  KEY `key_value_id` (`value_id`),
+  KEY `key_start_date` (`start_date`),
+  KEY `key_end_date` (`end_date`),
+  KEY `key_status` (`status`),
+  CONSTRAINT `fk_region_attribute__region` FOREIGN KEY (`object_id`) REFERENCES `region`(`object_id`),
+  CONSTRAINT `fk_region_attribute__entity_attribute` FOREIGN KEY (`entity_attribute_id`) REFERENCES entity_attribute (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Атрибуты региона';
+
+DROP TABLE IF EXISTS `region_string_value`;
+CREATE TABLE `region_string_value` (
+  `pk_id` BIGINT(20) NOT NULL AUTO_INCREMENT  COMMENT 'Суррогатный ключ',
+  `id` BIGINT(20) NOT NULL COMMENT 'Идентификатор локализации',
+  `locale_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор локали',
+  `value` VARCHAR(1000) COMMENT 'Текстовое значение',
+  PRIMARY KEY (`pk_id`),
+  UNIQUE KEY `unique_id__locale` (`id`,`locale_id`),
+  KEY `key_locale` (`locale_id`),
+  KEY `key_value` (`value`(128)),
+  CONSTRAINT `fk_region_string_value__locale` FOREIGN KEY (`locale_id`) REFERENCES `locale` (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Локализация атрибутов региона';
+
+-- ------------------------------
+-- City Type
+-- ------------------------------
+
+DROP TABLE IF EXISTS `city_type`;
+CREATE TABLE `city_type` (
+  `pk_id` BIGINT(20) NOT NULL AUTO_INCREMENT  COMMENT 'Суррогатный ключ',
+  `object_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор объекта',
+  `parent_id` BIGINT(20) COMMENT 'Идентификатор родительского объекта: не используется',
+  `parent_entity_id` BIGINT(20) COMMENT 'Идентификатор сущности родительского объекта',
+  `start_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата начала периода действия объекта',
+  `end_date` TIMESTAMP NULL DEFAULT NULL COMMENT 'Дата окончания периода действия объекта',
+  `status` INTEGER NOT NULL DEFAULT 1 COMMENT 'Статус',
+  `permission_id` BIGINT(20) NOT NULL DEFAULT 0 COMMENT 'Ключ прав доступа к объекту',
+  `external_id` VARCHAR(20) COMMENT 'Внешний идентификатор импорта записи',
+  PRIMARY KEY (`pk_id`),
+  UNIQUE KEY `unique_object_id__start_date` (`object_id`,`start_date`),
+  UNIQUE KEY `unique_external_id` (`external_id`,`start_date`),
+  KEY `key_object_id` (object_id),
+  KEY `key_parent_id` (`parent_id`),
+  KEY `key_parent_entity_id` (`parent_entity_id`),
+  KEY `key_start_date` (`start_date`),
+  KEY `key_end_date` (`end_date`),
+  KEY `key_status` (`status`),
+  KEY `key_permission_id` (`permission_id`),
+  CONSTRAINT `fk_city_type__entity` FOREIGN KEY (`parent_entity_id`) REFERENCES `entity` (`id`),
+  CONSTRAINT `fk_city_type__permission` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`permission_id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Тип населенного пункта';
+
+DROP TABLE IF EXISTS `city_type_attribute`;
+CREATE TABLE `city_type_attribute` (
+  `pk_id` BIGINT(20) NOT NULL AUTO_INCREMENT  COMMENT 'Суррогатный ключ',
+  `attribute_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор атрибута',
+  `object_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор объекта',
+  `entity_attribute_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор типа атрибута',
+  `value_id` BIGINT(20) COMMENT 'Идентификатор значения',
+  `start_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата начала периода действия атрибута',
+  `end_date` TIMESTAMP NULL DEFAULT NULL COMMENT 'Дата окончания периода действия атрибута',
+  `status` INTEGER NOT NULL DEFAULT 1 COMMENT 'Статус',
+  PRIMARY KEY (`pk_id`),
+  UNIQUE KEY `unique_id` (`attribute_id`,`object_id`,`entity_attribute_id`, `start_date`),
+  KEY `key_object_id` (`object_id`),
+  KEY `key_entity_attribute_id` (`entity_attribute_id`),
+  KEY `key_value_id` (`value_id`),
+  KEY `key_start_date` (`start_date`),
+  KEY `key_end_date` (`end_date`),
+  KEY `key_status` (`status`),
+  CONSTRAINT `fk_city_type_attribute__city_type` FOREIGN KEY (`object_id`) REFERENCES `city_type`(`object_id`),
+  CONSTRAINT `fk_city_type_attribute__entity_attribute` FOREIGN KEY (`entity_attribute_id`)
+  REFERENCES entity_attribute (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Атрибуты типа населенного пункта';
+
+DROP TABLE IF EXISTS `city_type_string_value`;
+CREATE TABLE `city_type_string_value` (
+  `pk_id` BIGINT(20) NOT NULL AUTO_INCREMENT  COMMENT 'Суррогатный ключ',
+  `id` BIGINT(20) NOT NULL COMMENT 'Идентификатор локализации',
+  `locale_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор локали',
+  `value` VARCHAR(1000) COMMENT 'Текстовое значение',
+  PRIMARY KEY (`pk_id`),
+  UNIQUE KEY `unique_id__locale` (`id`,`locale_id`),
+  KEY `key_locale` (`locale_id`),
+  KEY `key_value` (`value`(128)),
+  CONSTRAINT `fk_city_type_string_value__locale` FOREIGN KEY (`locale_id`) REFERENCES `locale` (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Локализация атрибутов типа населенного пункта';
+
+-- ------------------------------
+-- City
+-- ------------------------------
+DROP TABLE IF EXISTS `city`;
+CREATE TABLE `city` (
+  `pk_id` BIGINT(20) NOT NULL AUTO_INCREMENT  COMMENT 'Суррогатный ключ',
+  `object_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор объекта',
+  `parent_id` BIGINT(20) COMMENT 'Идентификатор родительского объекта',
+  `parent_entity_id` BIGINT(20) COMMENT 'Идентификатор сущности родительского объекта',
+  `start_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата начала периода действия объекта',
+  `end_date` TIMESTAMP NULL DEFAULT NULL COMMENT 'Дата окончания периода действия объекта',
+  `status` INTEGER NOT NULL DEFAULT 1 COMMENT 'Статус',
+  `permission_id` BIGINT(20) NOT NULL DEFAULT 0 COMMENT 'Ключ прав доступа к объекту',
+  `external_id` VARCHAR(20) COMMENT 'Внешний идентификатор импорта записи',
+  PRIMARY KEY (`pk_id`),
+  UNIQUE KEY `unique_object_id__start_date` (`object_id`,`start_date`),
+  UNIQUE KEY `unique_external_id` (`external_id`,`start_date`),
+  KEY `key_object_id` (`object_id`),
+  KEY `key_parent_id` (`parent_id`),
+  KEY `key_parent_entity_id` (`parent_entity_id`),
+  KEY `key_start_date` (`start_date`),
+  KEY `key_end_date` (`end_date`),
+  KEY `key_status` (`status`),
+  KEY `key_permission_id` (`permission_id`),
+  CONSTRAINT `ft_city__entity` FOREIGN KEY (`parent_entity_id`) REFERENCES `entity` (`id`),
+  CONSTRAINT `fk_city__permission` FOREIGN KEY (`permission_id`) REFERENCES `permission` (`permission_id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Населенный пункт';
+
+DROP TABLE IF EXISTS `city_attribute`;
+CREATE TABLE `city_attribute` (
+  `pk_id` BIGINT(20) NOT NULL AUTO_INCREMENT  COMMENT 'Суррогатный ключ',
+  `attribute_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор атрибута',
+  `object_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор объекта',
+  `entity_attribute_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор типа атрибута',
+  `value_id` BIGINT(20) COMMENT 'Идентификатор значения',
+  `start_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Дата начала периода действия атрибута',
+  `end_date` TIMESTAMP NULL DEFAULT NULL COMMENT 'Дата окончания периода действия атрибута',
+  `status` INTEGER NOT NULL DEFAULT 1 COMMENT 'Статус',
+  PRIMARY KEY  (`pk_id`),
+  UNIQUE KEY `unique_id` (`attribute_id`,`object_id`,`entity_attribute_id`, `start_date`),
+  KEY `key_object_id` (`object_id`),
+  KEY `key_entity_attribute_id` (`entity_attribute_id`),
+  KEY `key_value_id` (`value_id`),
+  KEY `key_start_date` (`start_date`),
+  KEY `key_end_date` (`end_date`),
+  KEY `key_status` (`status`),
+  CONSTRAINT `fk_city_attribute__city` FOREIGN KEY (`object_id`) REFERENCES `city`(`object_id`),
+  CONSTRAINT `fk_city_attribute__entity_attribute` FOREIGN KEY (`entity_attribute_id`) REFERENCES entity_attribute (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Атрибуты населенного пункта';
+
+DROP TABLE IF EXISTS `city_string_value`;
+CREATE TABLE `city_string_value` (
+  `pk_id` BIGINT(20) NOT NULL AUTO_INCREMENT  COMMENT 'Суррогатный ключ',
+  `id` BIGINT(20) NOT NULL COMMENT 'Идентификатор локализации',
+  `locale_id` BIGINT(20) NOT NULL COMMENT 'Идентификатор локали',
+  `value` VARCHAR(1000) COMMENT 'Текстовое значение',
+  PRIMARY KEY (`pk_id`),
+  UNIQUE KEY `unique_id__locale` (`id`,`locale_id`),
+  KEY `key_locale` (`locale_id`),
+  KEY `key_value` (`value`(128)),
+  CONSTRAINT `fk_city_string_value__locale` FOREIGN KEY (`locale_id`) REFERENCES `locale` (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT 'Локализация атрибутов населенного пункта';
+
+-- --------------------------------------------------------------------------------------------------------------------
+-- Domain
+-- --------------------------------------------------------------------------------------------------------------------
+
+-- todo jedani user profile
 
 
 
---
+++ jedani-user/src/main/java/ru/complitex/domain/entity/Entity.java
@@ -0,0 +1,8 @@
+package ru.complitex.domain.entity;
+
+/**
+ * @author Anatoly A. Ivanov
+ * 30.11.2017 14:27
+ */
+public class Entity {
+}
