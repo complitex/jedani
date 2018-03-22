dd8ca6511fb416d54e373c515b2d1b4b76cbf03f
Anatoly Ivanov <java@inhell.ru>
2018-03-22 16:58:54 +0700

    dev menu

--- jedani-worker/src/main/java/ru/complitex/jedani/worker/page/JedaniWebApplication.java
+++ jedani-worker/src/main/java/ru/complitex/jedani/worker/page/JedaniWebApplication.java
@@ -38,6 +38,8 @@ public class JedaniWebApplication extends WebApplication{
 
         configureBootstrap();
         configureMount();
+
+        getDebugSettings().setAjaxDebugModeEnabled(false);
     }
 
     private void configureBootstrap() {
