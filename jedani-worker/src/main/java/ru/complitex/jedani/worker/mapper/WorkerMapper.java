d66af8254dcb0e1e09a4d4149272001fac87d6ad
Anatoly Ivanov <java@inhell.ru>
2018-06-06 12:50:07 +0700

    dev worker node

--- jedani-worker/src/main/java/ru/complitex/jedani/worker/mapper/WorkerMapper.java
+++ jedani-worker/src/main/java/ru/complitex/jedani/worker/mapper/WorkerMapper.java
@@ -1,7 +1,11 @@
 package ru.complitex.jedani.worker.mapper;
 
 import org.apache.commons.lang3.StringUtils;
+import ru.complitex.common.entity.FilterWrapper;
 import ru.complitex.common.mybatis.BaseMapper;
+import ru.complitex.jedani.worker.entity.Worker;
+
+import java.util.List;
 
 /**
  * @author Anatoly A. Ivanov
@@ -18,6 +22,14 @@ public class WorkerMapper extends BaseMapper {
 
     public int getSubWorkersCount(Long objectId){
         return sqlSession().selectOne("selectSubWorkersCount", objectId);
+    }
+
+    public List<Worker> getWorkers(FilterWrapper<Worker> filterWrapper){
+        return sqlSession().selectList("selectWorkers", filterWrapper);
+    }
+
+    public Long getWorkersCount(FilterWrapper<Worker> filterWrapper){
+        return sqlSession().selectOne("selectWorkersCount", filterWrapper);
     }
 }
 
