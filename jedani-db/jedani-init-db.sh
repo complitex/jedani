3298aea6bc82ecdbfd1de32c212a1d4dffb0e7d4
Anatoly Ivanov <java@inheaven.ru>
2018-07-12 00:29:36 +0700

    update jedani_db

--- jedani-db/jedani-init-db.sh
+++ jedani-db/jedani-init-db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mysql -ujedani -pjedani jedani < jedani-create.sql
-mysql -ujedani -pjedani jedani < jedani-insert.sql
+mysql -ujedani -pjedani jedani_db < jedani-create.sql
+mysql -ujedani -pjedani jedani_db < jedani-insert.sql
