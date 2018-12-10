789ced9cb0c41b23fb0de49bd475c2afac5f849f
Anatoly Ivanov <java@inhell.ru>
2018-12-10 19:36:42 +0700

    add nomenclature code validation

--- jedani-worker/src/main/java/ru/complitex/jedani/worker/page/storage/NomenclatureEditPage.java
+++ jedani-worker/src/main/java/ru/complitex/jedani/worker/page/storage/NomenclatureEditPage.java
@@ -5,11 +5,15 @@ import org.apache.wicket.authroles.authorization.strategies.role.annotations.Aut
 import org.apache.wicket.model.Model;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
 import ru.complitex.address.entity.Country;
+import ru.complitex.common.entity.FilterWrapper;
 import ru.complitex.domain.component.form.AttributeSelectList;
 import ru.complitex.domain.entity.Attribute;
 import ru.complitex.domain.page.DomainEditPage;
+import ru.complitex.domain.service.DomainService;
 import ru.complitex.jedani.worker.entity.Nomenclature;
 import ru.complitex.jedani.worker.security.JedaniRoles;
+
+import javax.inject.Inject;
 
 /**
  * @author Anatoly A. Ivanov
@@ -17,6 +21,9 @@ import ru.complitex.jedani.worker.security.JedaniRoles;
  */
 @AuthorizeInstantiation(JedaniRoles.ADMINISTRATORS)
 public class NomenclatureEditPage extends DomainEditPage<Nomenclature> {
+    @Inject
+    private DomainService domainService;
+
     public NomenclatureEditPage(PageParameters parameters) {
         super(Nomenclature.class, parameters, NomenclatureListPage.class);
     }
@@ -28,5 +35,19 @@ public class NomenclatureEditPage extends DomainEditPage<Nomenclature> {
         }
 
         return null;
+    }
+
+    @Override
+    protected boolean validate(Nomenclature domain) {
+        Long count =  domainService.getDomainsCount(FilterWrapper.of(new Nomenclature()
+                .setText(Nomenclature.CODE, domain.getText(Nomenclature.CODE))));
+
+        if (count > 0){
+            error(getString("error_unique_nomenclature_code"));
+
+            return false;
+        }
+
+        return true;
     }
 }
