2e39d3bc3370dbf8caa9c25b3591225677045579
Anatoly A. Ivanov <java@inheaven.ru>
2019-09-14 02:30:54 +0700

    dev payment

--- jedani-db/update/20190909_1.0.29.sql
+++ jedani-db/update/20190909_1.0.29.sql
@@ -5,8 +5,8 @@ CALL createDomainTables('payment', 'Оплата');
 CALL createEntity(41, 'payment', 'Оплата', 'Оплата');
 CALL createEntityAttributeWithReference(41, 1, 11, 20, 'Профконсультант', 'Профконсультант');
 CALL createEntityAttribute(41, 2, 6, 'Дата', 'Дата');
-CALL createEntityAttribute(41, 3, 4, 'Сумма оплаты (в локальной валюте)', 'Сума оплати (в локальній валюті)');
-CALL createEntityAttribute(41, 4, 4, 'Сумма оплаты (в баллах)', 'Сума оплати (в балах)');
+CALL createEntityAttribute(41, 3, 4, 'Сумма (в локальной валюте)', 'Сума (в локальній валюті)');
+CALL createEntityAttribute(41, 4, 4, 'Сумма (в баллах)', 'Сума (в балах)');
 CALL createEntityAttributeWithReference(41, 5, 11, 28, 'Продажа', 'Продаж');
 CALL createEntityAttribute(41, 6, 4, 'Курс балла', 'Курс балла');
 
