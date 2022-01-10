fa77bf2258b2eed2cf5ee196db8ee1dd9527ac8f
Ivanov Anatoliy <anatoly@inheaven.ru>
2022-01-10 08:56:17 +0000

    fixed structure page

--- jedani-worker/src/main/java/ru/complitex/jedani/worker/page/worker/StructurePage.java
+++ jedani-worker/src/main/java/ru/complitex/jedani/worker/page/worker/StructurePage.java
@@ -76,7 +76,7 @@ public class StructurePage extends DomainListPage<Worker>{
     public StructurePage() {
         super(Worker.class, WorkerPage.class);
 
-        if (isStructure() && (!isAdmin() || !isStructureAdmin() || !isStructureView() || !isRegionalLeader())) {
+        if (isStructure() && (!isAdmin() && !isStructureAdmin() && !isStructureView() && !isRegionalLeader())) {
             throw new UnauthorizedInstantiationException(getClass());
         }
 
